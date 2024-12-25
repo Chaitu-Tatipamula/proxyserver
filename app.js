108db6dbc49c08fe14ac61c0ba06b231c8080ce4
Chaitu-Tatipamula <durga.chaitanya@quadbtech.com>
2024-12-26 03:34:05 +0530

    added deployed url to cors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require("cors")
 const app = express()
 app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: "https://assesment5-27dec.onrender.com",
       methods: "GET,POST,PUT,DELETE",
       credentials: true,
     })
